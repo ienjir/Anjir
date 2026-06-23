b9cf5f72096479c4153d3af66e37b252ccdf7309
ienjir <info@ienjir.dev>
2026-06-23 22:09:15 +0200

    Transfer backup

--- src/app/shared/gpx.ts
+++ src/app/shared/gpx.ts
@@ -1,1 +1,4 @@
-export function gpx_to_hike(gpx_string: string) {}
+export function gpx_to_hike(gpx_string: string) {
+  const dom_parser = new DOMParser();
+  const doc = dom_parser.parseFromString(gpx_string, 'application/xml');
+}
